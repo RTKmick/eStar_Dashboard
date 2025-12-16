1ee4473e66552b9eb8c1f7c95d994710eb7693c2
Mick <mick.sung01@gmail.com>
2025-12-16 17:36:20 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    // 請在這裡填入您最新的 Google Apps Script 網址
+    API_URL: "https://script.google.com/macros/s/AKfycbzEeAp8jubDbTaz8OJTLOvDd5NkgvXPQXUVURwklxzZ0Lzd2PrDSqWouIejcDifev9f/exec"
+};
